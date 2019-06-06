519b3599a86e0f29aac7d4bc77472c4676ba5b57
BrassM0nkeyy <mgrech97@gmail.com>
2019-06-06 00:19:07 -0400

    logic changes working prototype

--- index.js
+++ index.js
@@ -3,24 +3,34 @@ const bot = new Discord.Client();
 
 bot.login('');
 
+const desiredChannel = "Gulag";
 
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
     let newUserChannel = newMember.voiceChannel
     let oldUserChannel = oldMember.voiceChannel
 
-    //console.log(newUserChannel.name);
-
-    if( newUserChannel !== undefined && newUserChannel === undefined ){
-        //play music
-    }
-    //combine the 2 ifs with a big or
-    else if( newUserChannel !== undefined && oldUserChannel !== undefined && newUserChannel.name == 'Gulag' && oldUserChannel.name != newUserChannel.name ) {
-        //play music
+    if(newUserChannel && oldUserChannel && oldUserChannel.name != desiredChannel && newUserChannel.name == desiredChannel){
+        //music
         console.log('gulag');
-        console.log(newMember.voiceChannel.id);
+        console.log(newUserChannel.name);
+        newUserChannel.join()
+            .then( connect => {
+                const dispach = connect.playFile('./ear_rape.mp3');
+            });
     }
+
+    else if(!oldUserChannel && newUserChannel && newUserChannel.name == desiredChannel){
+        //music
+        console.log('gulag2');
+        newUserChannel.join()
+            .then( connect => {
+                const dispach = connect.playFile('./ear_rape.mp3');
+            });
+        //playingMusic = ture;
+    }
+
     else {
         console.log('no gulag');
-        console.log(newMember.voiceChannel.id);
+        //console.log(newMember.voiceChannel.id);
     }
 })
